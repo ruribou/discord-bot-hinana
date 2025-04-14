36a40363e5a8cd2370850d6bb89f0f4137a984e4
ruribou <ruribou@hotmail.com>
2025-04-14 21:36:14 +0900

    feat: とりあえずテストで動くように

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/ruribou/discord-bot-hinana/bot"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// ここで .env を読み込む
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	token := os.Getenv("DISCORD_BOT_TOKEN")
+	if token == "" {
+		log.Fatal("DISCORD_BOT_TOKEN is not set")
+	}
+
+	if err := bot.Start(token); err != nil {
+		log.Fatalf("failed to start bot: %v", err)
+	}
 }
